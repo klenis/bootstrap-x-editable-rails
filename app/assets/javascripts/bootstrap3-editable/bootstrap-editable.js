9e1efff7856b3b2e041ad48eec7aad603038d306
Emil <getninjan@gmail.com>
2017-10-19 22:39:11 +0200

    Hard code placement calculation
    Doesn't work with error "has no method getCalculatedOffset"

--- app/assets/javascripts/bootstrap3-editable/bootstrap-editable.js
+++ app/assets/javascripts/bootstrap3-editable/bootstrap-editable.js
@@ -4906,7 +4906,11 @@ Editableform based on Twitter Bootstrap 3
             }
 
 
-            var calculatedOffset = this.getCalculatedOffset(placement, pos, actualWidth, actualHeight);
+            // var calculatedOffset = this.getCalculatedOffset(placement, pos, actualWidth, actualHeight);
+            var calculatedOffset = placement == 'bottom' ? { top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2 } :
+placement == 'top' ? { top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2 } :
+placement == 'left' ? { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth } :
+/* placement == 'right' */ { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width };
 
             this.applyPlacement(calculatedOffset, placement);            
                      
